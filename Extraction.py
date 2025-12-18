3f6eb69ed92a383479f2fc7ca3f184ea1bd60cef
Rawan-Alnaggary <148374317+RawanElnaggary@users.noreply.github.com>
2025-12-18 12:37:21 +0200

    add comments in extraction

--- Extraction.py
+++ Extraction.py
@@ -6,29 +6,30 @@ import torch.nn as nn
 import torchvision.models as models
 import torchvision.transforms as transforms
 
+# Load the ResNet50 model
 resnet = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1)
-resnet.eval()
+resnet.eval() # Put the model in evaluation mode (only extracting features)
 
 featureExtractor = nn.Sequential (
-    *list(resnet.children())[:-1],
-    nn.Flatten()
+    *list(resnet.children())[:-1], # Take feature layers (All layers except the classification layer)
+    nn.Flatten() # Convert the output (2D/3D) into 1D feature vector
 )
 
 preprocess = transforms.Compose ([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
+    transforms.Resize((224, 224)), # ResNet50 expects
+    transforms.ToTensor(), # Convert the image into PyTorch tensor (multidimensional array for calculations)
     transforms.Normalize(
         mean=[0.485, 0.456, 0.406],
         std=[0.229, 0.224, 0.225]
-    )
+    ) # Standardize the color values
 ])
 
 def extract_resnet_features (img_path):
     img = Image.open(img_path).convert("RGB")
-    img = preprocess(img).unsqueeze(0)
-    with torch.no_grad():
-        features = featureExtractor(img).numpy().squeeze()
-    return features
+    img = preprocess(img).unsqueeze(0) # Apply preprocessing and add a batch dimension (PyTorch expects)
+    with torch.no_grad(): # No gradient calculation
+        features = featureExtractor(img).numpy().squeeze() # Convert to numpy array and remove the batch dimension
+    return features # 1D vector
 
 def load_dataset (path, split):
     X, y = [], []
